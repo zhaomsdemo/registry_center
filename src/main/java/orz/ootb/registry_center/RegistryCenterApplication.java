9c4ba0df1f7b7de83be18c68374c1cc114a392c6
Jianhua Zhao <Jiazhao@gap.com>
2018-10-09 16:52:45 -0700

    init

---
+++ src/main/java/orz/ootb/registry_center/RegistryCenterApplication.java
@@ -0,0 +1,14 @@
+package orz.ootb.registry_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryCenterApplication.class, args);
+    }
+}
